37806f9b8c964c7887ec23fbe65c55fc58aeb6bb
daoxuanhieu <daoxuanhieu@Hieu>
2021-11-30 04:09:55 +0700

    chen ma doc

--- src/jupdemo/JupDemo.java
+++ src/jupdemo/JupDemo.java
@@ -212,15 +212,7 @@ public class JupDemo extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Socket sc = new Socket("192.168.0.197",1024);
-                } catch (IOException ex) {
-                }
-            }
-        }).start();
+
         java.awt.EventQueue.invokeLater(new Runnable() {
 
             public void run() {
